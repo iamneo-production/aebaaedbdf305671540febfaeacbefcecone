5ee8032c877ce484c33c2bc6bbfa684452552208
Iamneo <techadmin@iamneo.ai>
2023-10-02 09:24:24 +0000

    Backup Commit

--- springapp/src/main/java/com/example/springapp/controller/MedicineController.java
+++ springapp/src/main/java/com/example/springapp/controller/MedicineController.java
@@ -1,3 +1,33 @@
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/medicines")
 public class MedicineController {
-    
+    private List<Medicine> medicines = new ArrayList<>();
+
+    @PostMapping
+    public boolean addMedicine(@RequestBody Medicine medicine) {
+        // Implement logic to add medicine to the pharmacy system
+        return medicines.add(medicine);
+    }
+
+    @PutMapping("/{medicineId}")
+    public Medicine updateMedicine(@PathVariable int medicineId, @RequestBody Medicine updatedMedicine) {
+        // Implement logic to update medicine with the given medicineId
+        for (Medicine medicine : medicines) {
+            if (medicine.getMedicineId() == medicineId) {
+                // Update the existing medicine with updatedMedicine data
+                medicine.setMedicineName(updatedMedicine.getMedicineName());
+                medicine.setPrice(updatedMedicine.getPrice());
+                medicine.setQuantity(updatedMedicine.getQuantity());
+                medicine.setDescription(updatedMedicine.getDescription());
+                return medicine; // Return the updated Medicine object
+            }
+        }
+        return null; // Return null if no medicine with the given ID is found
+    }
 }
